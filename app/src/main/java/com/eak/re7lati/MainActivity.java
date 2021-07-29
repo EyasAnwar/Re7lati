e97de3b23f403bae544e6de50e74cfd55db76f48
EyasAnwar <eyasanwar@gmail.com>
2021-07-29 21:43:06 +0300

    init commit

---
+++ app/src/main/java/com/eak/re7lati/MainActivity.java
@@ -0,0 +1,14 @@
+package com.eak.re7lati;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
